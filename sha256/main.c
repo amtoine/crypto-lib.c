48bc6e39b68b305ddef8cca59690b3c44bbedae8
amtoine <stevan.antoine@gmail.com>
2025-02-22 15:28:13 +0100

    move to `sha256/`

---
+++ sha256/main.c
@@ -0,0 +1,9 @@
+#include <stdlib.h>
+
+#include "sha256.h"
+
+int main(int argc, char *argv[]) {
+    sha256_hash();
+
+    return EXIT_SUCCESS;
+}
---
+++ sha256/sha256.h
@@ -0,0 +1,3 @@
+#pragma once
+
+void sha256_hash();
