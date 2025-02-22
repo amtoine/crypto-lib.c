756afc9db569c7392a1dd03bf1821c9292f45bb3
amtoine <stevan.antoine@gmail.com>
2025-02-22 16:29:05 +0100

    take input from CLI in `main.c`

--- sha256/main.c
+++ sha256/main.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/types.h>
 
 #include "sha256.h"
 
 int main(int argc, char *argv[]) {
-    u_int8_t input[] = {'f', 'o', 'o'};
-    u_int64_t size = 3;
+    if (argc < 2) {
+        fprintf(stderr, "expected one positional argument\n");
+        exit(EXIT_FAILURE);
+    }
 
-    u_int32_t *hash = sha256_hash(size, input);
+    u_int32_t *hash = sha256_hash(strlen(argv[1]), (u_int8_t *)argv[1]);
 
     printf("%08x%08x%08x%08x%08x%08x%08x%08x\n", hash[0], hash[1], hash[2], hash[3], hash[4],
            hash[5], hash[6], hash[7]);
